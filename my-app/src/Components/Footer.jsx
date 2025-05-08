b3dd68ae63acb250e55b525f7e0ac2f417f1bcab
DarkLight-DL <a.asikahamedm@gmail.com>
2025-05-08 15:24:05 +0530

    new

--- my-app/src/Components/Footer.jsx
+++ my-app/src/Components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <Box sx={{height:'20dvh',width:'auto',textAlign:'center',alignContent:'center'}}>
 
         <Typography sx={{fontSize:{xs:'8px',sm:'13px'}}}> 
-            Copyright &copy; {new Date().getFullYear()} All rights reserved | This template is made with ❤️ by asik ahamed
+            Copyright &copy; {new Date().getFullYear()} All rights reserved | This template is made with ❤️ by Asik Ahamed
         </Typography>
       </Box>
     </>
